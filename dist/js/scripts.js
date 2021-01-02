5ecda34e6b4f847ecaa6f93d276ec22a46bdaf73
Ciaran <ciarankamp@gmail.com>
2021-01-02 17:21:22 +0000

    update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,36 +1,8 @@
 'use strict';
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
-  }
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-function _asyncToGenerator(fn) {
-  return function () {
-    var self = this,
-      args = arguments;
-    return new Promise(function (resolve, reject) {
-      var gen = fn.apply(self, args);
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'next', value);
-      }
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'throw', err);
-      }
-      _next(undefined);
-    });
-  };
-}
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOM fully loaded and parsed');
@@ -41,9 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     return local.toJSON().slice(0, 10);
   };
 
-  document.getElementById(
-    'date-selector'
-  ).value = new Date().toDateInputValue(); // variables
+  document.getElementById('date-selector').value = new Date().toDateInputValue(); // variables
 
   var dateSelector = document.getElementById('date-selector');
   var todaysDate = dateSelector.value;
@@ -63,17 +33,13 @@ document.addEventListener('DOMContentLoaded', function () {
       displayArticles();
     }
   });
-  document
-    .getElementById('previous-page')
-    .addEventListener('click', function () {
-      dateSelector.value = incDecDate(-1, dateSelector.value);
-      displayArticles();
-    });
-  document
-    .getElementById('show-articles')
-    .addEventListener('click', function () {
-      displayArticles();
-    });
+  document.getElementById('previous-page').addEventListener('click', function () {
+    dateSelector.value = incDecDate(-1, dateSelector.value);
+    displayArticles();
+  });
+  document.getElementById('show-articles').addEventListener('click', function () {
+    displayArticles();
+  });
   displayArticles(); // functions
 
   function clearPrevious() {
@@ -114,39 +80,34 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function _displayArticles() {
-    _displayArticles = _asyncToGenerator(
-      /*#__PURE__*/ regeneratorRuntime.mark(function _callee() {
-        var dateArray;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch ((_context.prev = _context.next)) {
-              case 0:
-                clearPrevious();
-                imageCount = 1;
-                dateArray = createDateArray();
-                _context.next = 5;
-                return addRemoteDataToArray(dateArray);
+    _displayArticles = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var dateArray;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              clearPrevious();
+              imageCount = 1;
+              dateArray = createDateArray();
+              _context.next = 5;
+              return addRemoteDataToArray(dateArray);
 
-              case 5:
-                resultsArray = _context.sent;
-                console.log(
-                  '🚀 ~ file: scripts.js ~ line 94 ~ displayArticles ~ resultsArray',
-                  resultsArray
-                );
-                resultsArray = resultsArray.reverse();
-                resultsArray.forEach(function (element) {
-                  createNewElements(element);
-                });
-                showArticles();
+            case 5:
+              resultsArray = _context.sent;
+              console.log('🚀 ~ file: scripts.js ~ line 94 ~ displayArticles ~ resultsArray', resultsArray);
+              resultsArray = resultsArray.reverse();
+              resultsArray.forEach(function (element) {
+                createNewElements(element);
+              });
+              showArticles();
 
-              case 10:
-              case 'end':
-                return _context.stop();
-            }
+            case 10:
+            case "end":
+              return _context.stop();
           }
-        }, _callee);
-      })
-    );
+        }
+      }, _callee);
+    }));
     return _displayArticles.apply(this, arguments);
   }
 
@@ -165,71 +126,62 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function _addRemoteDataToArray() {
-    _addRemoteDataToArray = _asyncToGenerator(
-      /*#__PURE__*/ regeneratorRuntime.mark(function _callee2(dateArray) {
-        var results, _loop, index;
+    _addRemoteDataToArray = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(dateArray) {
+      var results, _loop, index;
 
-        return regeneratorRuntime.wrap(function _callee2$(_context3) {
-          while (1) {
-            switch ((_context3.prev = _context3.next)) {
-              case 0:
-                results = [];
-                _loop = /*#__PURE__*/ regeneratorRuntime.mark(function _loop(
-                  index
-                ) {
-                  var date;
-                  return regeneratorRuntime.wrap(function _loop$(_context2) {
-                    while (1) {
-                      switch ((_context2.prev = _context2.next)) {
-                        case 0:
-                          date = dateArray[index];
-                          _context2.next = 3;
-                          return fetch(url + apiKey + '&date=' + date).then(
-                            function (response) {
-                              return response.json().then(function (data) {
-                                results[index] = data;
-                              });
-                            }
-                          );
+      return regeneratorRuntime.wrap(function _callee2$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+              results = [];
+              _loop = /*#__PURE__*/regeneratorRuntime.mark(function _loop(index) {
+                var date;
+                return regeneratorRuntime.wrap(function _loop$(_context2) {
+                  while (1) {
+                    switch (_context2.prev = _context2.next) {
+                      case 0:
+                        date = dateArray[index];
+                        _context2.next = 3;
+                        return fetch(url + apiKey + '&date=' + date).then(function (response) {
+                          return response.json().then(function (data) {
+                            results[index] = data;
+                          });
+                        });
 
-                        case 3:
-                        case 'end':
-                          return _context2.stop();
-                      }
+                      case 3:
+                      case "end":
+                        return _context2.stop();
                     }
-                  }, _loop);
-                });
-                index = 0;
+                  }
+                }, _loop);
+              });
+              index = 0;
 
-              case 3:
-                if (!(index < dateArray.length)) {
-                  _context3.next = 8;
-                  break;
-                }
-
-                return _context3.delegateYield(_loop(index), 't0', 5);
-
-              case 5:
-                index++;
-                _context3.next = 3;
+            case 3:
+              if (!(index < dateArray.length)) {
+                _context3.next = 8;
                 break;
+              }
 
-              case 8:
-                return _context3.abrupt(
-                  'return',
-                  new Promise(function (resolve) {
-                    resolve(results);
-                  })
-                );
+              return _context3.delegateYield(_loop(index), "t0", 5);
 
-              case 9:
-              case 'end':
-                return _context3.stop();
-            }
+            case 5:
+              index++;
+              _context3.next = 3;
+              break;
+
+            case 8:
+              return _context3.abrupt("return", new Promise(function (resolve) {
+                resolve(results);
+              }));
+
+            case 9:
+            case "end":
+              return _context3.stop();
           }
-        }, _callee2);
-      })
-    );
+        }
+      }, _callee2);
+    }));
     return _addRemoteDataToArray.apply(this, arguments);
   }
 
@@ -242,20 +194,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function createNewElements(data) {
     console.log('called create new');
-    gridArea.insertAdjacentHTML(
-      'beforeend',
-      '<article id="article' +
-        imageCount +
-        '" class="article"><a target="_blank" id="image-link' +
-        imageCount +
-        '" data-fancybox="gallery" class="image-link"><img id="img' +
-        imageCount +
-        '" src="" class="images"></a><a target="_blank" id="title-link' +
-        imageCount +
-        '" class=""title-link><h5 id="title' +
-        imageCount +
-        '" class="title"></h5></a></article>'
-    );
+    gridArea.insertAdjacentHTML('beforeend', '<article id="article' + imageCount + '" class="article"><a target="_blank" id="image-link' + imageCount + '" data-fancybox="gallery" class="image-link"><img id="img' + imageCount + '" src="" class="images"></a><a target="_blank" id="title-link' + imageCount + '" class=""title-link><h5 id="title' + imageCount + '" class="title"></h5></a></article>');
     var postUrl = data.hdurl;
     var articleTitleContent = data.title;
     var articleImageContent = data.url;
@@ -272,9 +211,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (articleImageContent != null) {
       articleImageArea.setAttribute('src', articleImageContent);
     } else {
-      articleImageArea
-        .setAttribute('style', placeHolderImageAreaDimensions)
-        .setAttribute('src', placeHolderImage);
+      articleImageArea.setAttribute('style', placeHolderImageAreaDimensions).setAttribute('src', placeHolderImage);
     }
 
     imageCount++;
