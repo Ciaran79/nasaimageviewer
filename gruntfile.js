37a763fc79b6c7752aeebaef148cfd170b5b8876
Ciaran <ciarankamp@gmail.com>
2021-01-02 17:14:26 +0000

    updates

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
     postcss: {
       options: {
         // map: false, // inline sourcemaps
@@ -9,18 +9,18 @@ module.exports = function (grunt) {
         // // or
         map: {
           inline: false, // save all sourcemaps as separate files...
-          annotation: "dist/css/", // ...to the specified directory
+          annotation: 'dist/css/', // ...to the specified directory
         },
 
         processors: [
-          require("pixrem")(), // add fallbacks for rem units
-          require("autoprefixer")({ browsers: "last 2 versions" }), // add vendor prefixes
-          require("cssnano")(), // minify the result
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({ browsers: 'last 2 versions' }), // add vendor prefixes
+          require('cssnano')(), // minify the result
         ],
       },
       dist: {
-        src: "src/css/style.css",
-        dest: "dist/css/style.css",
+        src: 'src/css/style.css',
+        dest: 'dist/css/style.css',
       },
     },
     babel: {
@@ -28,42 +28,42 @@ module.exports = function (grunt) {
         sourceMap: true,
         presets: [
           [
-            "@babel/preset-env",
+            '@babel/preset-env',
             {
-              useBuiltIns: "entry",
+              // useBuiltIns: "entry",
             },
           ],
         ],
       },
       dist: {
         files: {
-          "dist/js/scripts.js": "src/js/scripts.js",
+          'dist/js/scripts.js': 'src/js/scripts.js',
         },
       },
     },
     watch: {
       scripts: {
-        files: ["src/scss/style.scss", "src/js/scripts.js"],
-        tasks: ["sass", "postcss", "babel"],
+        files: ['src/scss/style.scss', 'src/js/scripts.js'],
+        tasks: ['sass', 'postcss', 'babel'],
       },
     },
     sass: {
       dist: {
         files: {
-          "src/css/style.css": "src/scss/style.scss",
+          'src/css/style.css': 'src/scss/style.scss',
         },
       },
     },
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-postcss");
-  grunt.loadNpmTasks("grunt-babel");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask("default", ["sass", "postcss", "babel"]);
-  grunt.registerTask("build", ["sass", "postcss", "babel"]);
+  grunt.registerTask('default', ['sass', 'postcss', 'babel']);
+  grunt.registerTask('build', ['sass', 'postcss', 'babel']);
 };
