622cf5a8ce9a9d51736d40dca5b5f810b27e03cf
Ciaran <ciarankamp@gmail.com>
2021-09-03 15:41:03 +0100

    tidy up

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
- const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
